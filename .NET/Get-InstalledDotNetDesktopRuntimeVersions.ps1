38657e02653a0b9417120479169423718e63ad54
John Gage Faulkner <46038619+johngagefaulkner@users.noreply.github.com>
2024-01-06 16:41:12 -0500

    Add files via upload

---
+++ .NET/Get-InstalledDotNetDesktopRuntimeVersions.ps1
@@ -0,0 +1,20 @@
+﻿$ProgressPreference = 'SilentlyContinue'
+$ErrorActionPreference = 'Stop'
+Clear-Host
+Write-Host "[ Get-InstalledDotNetDesktopRuntimeVersions.ps1 ]"
+Write-Host "Obtaining a list of all installed .NET Desktop App Runtime versions, please wait... "
+Write-Host
+
+function Get-InstalledDotNetDesktopRuntimeVersions() {
+    $installedVersions = Get-ChildItem -Path "C:\Program Files\dotnet\shared\Microsoft.WindowsDesktop.App" | Select-Object -ExpandProperty Name
+    return $installedVersions
+}
+
+$locallyInstalledVersions = Get-InstalledDotNetDesktopRuntimeVersions
+
+foreach ($dotNetRuntime in $locallyInstalledVersions) {
+    Write-Host " • Installed: " -NoNewline
+    Write-Host $dotNetRuntime -ForegroundColor Green
+}
+
+Write-Host
